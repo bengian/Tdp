1d8be9c28f1eb1e7082d1133877783b781f2ef43
Ben <Ben@192.168.1.7>
2016-04-06 20:29:03 +0200

    aggiunto una variabili inutile

--- src/it/polito/tdp/parole/ParoleController.java
+++ src/it/polito/tdp/parole/ParoleController.java
@@ -19,7 +19,7 @@ import javafx.scene.control.TextField;
 
 public class ParoleController {
 	WordSet ws=new WordSet();
-	
+	int lol=0;
 	Parole elenco ;
 
     @FXML // ResourceBundle that was given to the FXMLLoader
